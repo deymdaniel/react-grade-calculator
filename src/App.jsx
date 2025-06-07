f64f1bf8808598bf62abd2d4882abfc4c6dc58c4
deymdaniel <danielbiacaniii@gmail.com>
2025-06-07 11:20:43 +0800

    fixed final grade computation

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,49 @@ const App = () => {
   const [final, setFinal] = useState(0);
   const [grade, setGrade] = useState("");
   const [finalGrade, setFinalGrade] = useState("");
-  const calculateGrade = () => {
-    setGrade(quiz * 0.3 + lab * 0.3 + final * 0.4).toFixed(2);
-  };
+  const calculateAndSetGrades = () => {
+    const computedGrade = quiz * 0.3 + lab * 0.3 + final * 0.4;
+    setGrade(computedGrade);
 
-  const calculateFinalGrade = () => {
-    const numericGrade = grade;
     let calculatedFinalGrade = "";
-    if (numericGrade >= 98.51 && numericGrade <= 100) {
+    const gradeNum = Number(computedGrade);
+
+    if (gradeNum >= 98.51 && gradeNum <= 100) {
       calculatedFinalGrade = "4.0";
-    } else if (numericGrade >= 96.51 && numericGrade <= 98.5) {
+    } else if (gradeNum >= 96.51 && gradeNum <= 98.5) {
       calculatedFinalGrade = "3.75";
-    } else if (numericGrade >= 94.51 && numericGrade <= 96.5) {
+    } else if (gradeNum >= 94.51 && gradeNum <= 96.5) {
       calculatedFinalGrade = "3.5";
-    } else if (numericGrade >= 92.51 && numericGrade <= 94.5) {
+    } else if (gradeNum >= 92.51 && gradeNum <= 94.5) {
       calculatedFinalGrade = "3.25";
-    } else if (numericGrade >= 90.51 && numericGrade <= 92.5) {
+    } else if (gradeNum >= 90.51 && gradeNum <= 92.5) {
       calculatedFinalGrade = "3.0";
-    } else if (numericGrade >= 88.51 && numericGrade <= 90.5) {
+    } else if (gradeNum >= 88.51 && gradeNum <= 90.5) {
       calculatedFinalGrade = "2.75";
-    } else if (numericGrade >= 86.51 && numericGrade <= 88.5) {
+    } else if (gradeNum >= 86.51 && gradeNum <= 88.5) {
       calculatedFinalGrade = "2.5";
-    } else if (numericGrade >= 84.51 && numericGrade <= 86.5) {
+    } else if (gradeNum >= 84.51 && gradeNum <= 86.5) {
       calculatedFinalGrade = "2.25";
-    } else if (numericGrade >= 82.51 && numericGrade <= 84.5) {
+    } else if (gradeNum >= 82.51 && gradeNum <= 84.5) {
       calculatedFinalGrade = "2.0";
-    } else if (numericGrade >= 80.51 && numericGrade <= 82.5) {
+    } else if (gradeNum >= 80.51 && gradeNum <= 82.5) {
       calculatedFinalGrade = "1.75";
-    } else if (numericGrade >= 78.51 && numericGrade <= 80.5) {
+    } else if (gradeNum >= 78.51 && gradeNum <= 80.5) {
       calculatedFinalGrade = "1.5";
-    } else if (numericGrade >= 76.51 && numericGrade <= 78.5) {
+    } else if (gradeNum >= 76.51 && gradeNum <= 78.5) {
       calculatedFinalGrade = "1.25";
-    } else if (numericGrade >= 74.51 && numericGrade <= 76.5) {
+    } else if (gradeNum >= 74.51 && gradeNum <= 76.5) {
       calculatedFinalGrade = "1.0";
-    } else if (numericGrade >= 50 && numericGrade <= 74.5) {
+    } else if (gradeNum >= 50 && gradeNum <= 74.5) {
       calculatedFinalGrade = "0.0";
-    } else if (numericGrade < 50 && numericGrade >= 0) {
+    } else if (gradeNum < 50 && gradeNum >= 0) {
       calculatedFinalGrade = "0.0";
     } else {
       calculatedFinalGrade = "N/A";
     }
     setFinalGrade(calculatedFinalGrade);
   };
+
   return (
     <div className='bg-white p-4 rounded-lg shadow-md flex flex-col items-center justify-center h-screen'>
       <div className='flex flex-col justify-center items-center h-min bg-gray-100 border border-gray-200 rounded-4xl w-min p-12 gap-6'>
@@ -88,14 +89,14 @@ const App = () => {
         <button
           className='bg-blue-500 text-white rounded-md p-2'
           onClick={() => {
-            calculateGrade();
-            calculateFinalGrade();
+            calculateAndSetGrades();
           }}
         >
           Calculate
         </button>
         <p>Grade: {grade}</p>
         <p>Final Grade: {finalGrade}</p>
+        <button className='bg-red-500 text-white rounded-md p-2'>Logout</button>
       </div>
     </div>
   );
